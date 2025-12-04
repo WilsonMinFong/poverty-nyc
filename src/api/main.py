2134f29ac4e170bc99813c438f6867de21c48173
Wilson Fong <wilsonmfong@gmail.com>
2025-12-04 15:15:20 -0500

    Add ALLOWED_ORIGINS for deployment

--- src/api/main.py
+++ src/api/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.api.routes import router
+import os
 
 app = FastAPI(title="NYC Food Gap Visualization API")
+origins = os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",")
 
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite default port
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
